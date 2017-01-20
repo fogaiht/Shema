1eb7b532ccfc5d69b7359b80774124d39c8a4702
fogaiht <thiago_f_pf@hotmail.com>
2017-01-20 21:08:54 -0200

    Faq class and webView

--- app/src/main/java/fogaiht/shema/Faq.java
+++ app/src/main/java/fogaiht/shema/Faq.java
@@ -1,7 +1,33 @@
 package fogaiht.shema;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 /**
  * Created by thiago on 20/01/17.
  */
-public class Faq {
+public class Faq extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.site);
+
+//        WebView webview = (WebView) findViewById(R.id.wv);
+
+//        webview.loadUrl("http://shemadeuschama.wixsite.com/shema");
+
+
+        String url ="http://shemadeuschama.wixsite.com/shema";
+        WebView view = (WebView)this.findViewById(R.id.wv);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+        view.setWebViewClient(new MyBrowser());
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url); return true;
+        }
+    }
 }

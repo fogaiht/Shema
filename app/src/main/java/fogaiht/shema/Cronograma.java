c6b913bb4cc21a6260a50f27bfafbfad0505846f
fogaiht <thiago_f_pf@hotmail.com>
2017-03-08 20:46:56 -0300

    Main Done and add new icons

--- app/src/main/java/fogaiht/shema/Cronograma.java
+++ app/src/main/java/fogaiht/shema/Cronograma.java
@@ -1,16 +1,14 @@
 package fogaiht.shema;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.support.v7.widget.Toolbar;
+
 /**
  * Created by thiago on 20/01/17.
  */
@@ -43,25 +41,14 @@ public class Cronograma extends AppCompatActivity{
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-
         // Assign adapter to ListView
         listView.setAdapter(adapter);
 
         // ListView Item Click Listener
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-//            @Override
-//            public void bindView(View convertView, Context arg1, Cursor cursor) {
-//                if(isB)
-//                    convertView.setBackgroundColor(Color.RED);
-//                else
-//                    convertView.setBackgroundColor(Color.BLACK);}
-
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0) {
-
-
                     startActivity(new Intent(getBaseContext(), Sabado.class));
                 } else if (position == 1) {
                     startActivity(new Intent(getBaseContext(), Domingo.class));
--- app/src/main/java/fogaiht/shema/MainActivity.java
+++ app/src/main/java/fogaiht/shema/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -35,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_scroll);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ImageButton iniciar = (ImageButton) findViewById(R.id.mapa);
         iniciar.setOnClickListener(new View.OnClickListener() {
